9b6376cb3a4fa7473fbf1433ad05165a00d2e0c5
Kim jihyun <j.hyun2658@gmail.com>
2023-03-10 11:38:17 +0900

    java projct - day04 삼항연산자, scanner

--- src/kr/ac/kopo/day04/OperationMain05.java
+++ src/kr/ac/kopo/day04/OperationMain05.java
@@ -1,5 +1,14 @@
 package kr.ac.kopo.day04;
 
 public class OperationMain05 {
+	public static void main(String[] srgs) {
+		
+		int num = 0x0055ff00;
+		System.out.printf("%08X\n", num); // 16진수 표현
+
+		System.out.printf("%08X\n", ~num); // 16진수 뒤집기
+		System.out.printf("%08X\n", num & 0xffff0000); // 16진수 & 하위 4개 0으로 초기화
+		System.out.printf("%08X\n", num | 0x0000ffff); // 16진수 & 하위 4개 f로 초기화
+	}
 
 }
---
+++ src/kr/ac/kopo/day04/ScannerMain.java
@@ -0,0 +1,33 @@
+package kr.ac.kopo.day04;
+
+import java.util.Scanner; //(1.5버전 이후 사용가능)
+
+public class ScannerMain {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("문자를 입력 : ");
+		
+		char ch = sc.nextLine().charAt(0);
+		System.out.println("ch : " + ch);
+		
+		System.out.print("문자열을 입력 : ");
+//		String str = sc.next(); // sc.next() - 한 단어 별로 인식
+		String str = sc.nextLine(); // sc.nextLine() - 한 문장 단위 인식
+		
+		System.out.println("str : "+  str);
+		
+		System.out.print("정수를 입력 : ");
+		int num = sc.nextInt();
+		
+		System.out.println("num : " + num);
+	
+		System.out.print("실수를 입력 : ");
+		double num2 = sc.nextDouble();
+		
+		System.out.println("num2 = " + num2);
+		
+		
+	}
+
+}
