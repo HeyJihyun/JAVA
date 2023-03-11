f29a11989cecdf5fa10834edc2b23eea61d3dde7
Kim jihyun <j.hyun2658@gmail.com>
2023-03-11 12:38:08 +0900

    java 과제 03 수정

--- src/kr/ac/kopo/day04/homework/Homework03.java
+++ src/kr/ac/kopo/day04/homework/Homework03.java
@@ -10,33 +10,34 @@ public class Homework03 {
 		int num1 = sc.nextInt();
 		int num2 = sc.nextInt();
 		int num3 = sc.nextInt();
-		int tmp;
-		
-		if(num1 > num2) {
-			tmp = num1; 
-			if(num2 > num3) { // num1 > num2 > num3 
+		int tmp = num1;	
+
+		if(num1 > num2 && num1 > num3) { // num1이 가장 클 때
+			
+			if(num2 > num3) { // num3 < num2 < num1
 				num1 = num3; 
-				num3 = tmp; 
-			} else if(num1 > num3) { // num1 > num3 > num2 
+			} else { // num2 < num3 < num1
 				num1 = num2;
 				num2 = num3;
-				num3 = tmp;
-			} else { // num3 > num1 > num2 
-				num1 = num2;
-				num2 = tmp;
-			}		
-		} else { // num2 > num1  
+			}	
+			num3 = tmp; 
+			
+		} else if(num2 > num3){ // num2가 가장 클때
 			tmp = num2; 
-			if(num1 > num3) { // num2 > num1 > num3 
+
+			if(num1 > num3) { // num3 < num1 < num2
 				num2 = num1;
 				num1 = num3;
-				num3 = tmp;
-			} else if(num2 > num3) { // num2 > num3 > num1 
+			} else { // num1 < num3 < num2 
 				num2 = num3;
-				num3 = tmp;
 			}
-			// num3 > num2 > num1 - 변수 변화 없음
-		}
+			num3 = tmp; 
+			
+		} else if(num1 > num2) { //num2 < num1 < num3
+			num1 = num2;
+			num2 = tmp; 
+		} // 모두 아닐 때 그냥 출력 num1 < num2 < num3
+		
 		System.out.printf("%d %d %d", num1, num2, num3);
 	}
 
